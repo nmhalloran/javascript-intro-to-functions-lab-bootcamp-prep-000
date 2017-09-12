153d3809195e1664d0720bbbf916541f8be0912f
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-12 02:11:03 +0000

    automatically backed up by learn

---
+++ poop.rb
@@ -0,0 +1,7 @@
+def greeting(name = "Ruby Programmer", language ="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting
+
+greeting("Nick", "Javascript")
